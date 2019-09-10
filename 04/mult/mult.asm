aae714e33aab2898576139dc36a2c5100b4a6882
Seoungil Kang <seoungil.kang@lge.com>
2019-09-10 21:37:27 +0900

    add mult

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,22 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+        @R2       
+        M = 0     // R2 = 0
+(LOOP)
+        @R0
+        D = M     // D = R0
+        D = D - 1 // R0 = R0 - 1
+        @END
+        D;JLT     // If (R0-1)<0 goto END
+        @R1
+        D = M
+        @R2
+        M = M + D // R2 = R2 + R1
+        @R0
+        M = M - 1 // R0 = R0 - 1
+        @LOOP
+        0;JMP     // Goto LOOP
+(END)
+        @END
+        0;JMP
